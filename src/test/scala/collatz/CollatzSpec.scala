e550f4f4d9db7c98ec8a4a60b5c23e101a0baf1e
Ryder Mitchell <60051227+rmitchellPitzer@users.noreply.github.com>
2023-02-14 09:20:39 +0000

    Tests might not work

--- src/test/scala/collatz/CollatzSpec.scala
+++ src/test/scala/collatz/CollatzSpec.scala
@@ -3,6 +3,29 @@ import org.scalatest.matchers.should._
 
 class CollatzSpec extends AnyFunSuite with Matchers:
 
-// write tests here!
+    test("1") {
+        Collatz(1) should be (4)
+    }
+
+    test("2") {
+        Collatz(2) should be (1)
+    }
+
+    test("3") {
+        Collatz(3) should be (10)
+    }
+
+    test("8") {
+        Collatz(8) should be (4)
+    }
+
+    test("9") {
+        Collatz(9) should be (28)
+    }
+    
+    test("0") {
+        Collatz(0) should be (0)
+    }
+
 
 end CollatzSpec
